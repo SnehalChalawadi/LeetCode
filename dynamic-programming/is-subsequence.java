fdffc7b874e9dbb53fdf1cd14a3e7aed91d470f5
SnehalChalawadi <snehalchalawadi2005@gmail.com>
2026-09-07 14:56:23 +0530

    LC-is-subsequence [Runtime: N/A; Memory: N/A]

--- dynamic-programming/is-subsequence.java
+++ dynamic-programming/is-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         int i=0;
         for(int j=0;j<t.length();j++)
         {
-            if(s.charAt(i)==t.charAt(j) && (!s.isEmpty()))
+            if((!s.isEmpty()) && s.charAt(i)==t.charAt(j))
             {
                 i++;
             }
