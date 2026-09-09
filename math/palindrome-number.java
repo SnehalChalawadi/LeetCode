761820220f2eb93bf92970a6530cb7adae163a02
SnehalChalawadi <snehalchalawadi2005@gmail.com>
2026-09-09 18:48:13 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

---
+++ math/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n=x;
+        int rev=0;
+        while(n>0)
+        {
+            rev=rev*10 + n%10;
+            n=n/10;
+        }
+        if(x == rev)
+        {
+            return true;
+        }
+        return false;
+    }
+}
