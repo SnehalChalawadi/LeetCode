ec036ac2b4fc2b3b5cd00b995844944695bd8725
SnehalChalawadi <snehalchalawadi2005@gmail.com>
2026-09-10 20:09:02 +0530

    LC-smallest-stable-index-i [Runtime: N/A; Memory: N/A]

---
+++ array/smallest-stable-index-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++)
+        {
+            int max=nums[i];
+            int min=nums[i];
+            int j=0;
+            while(j<=0)
+            {
+                max=Math.max(max,nums[j]);
+                j++;
+            }
+            j=nums.length-1;
+            while(j>=i){
+                min=Math.min(min,nums[j]);
+                j--;
+            }
+            int score=max-min;
+            if(score<=k)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
