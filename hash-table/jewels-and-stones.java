fa91fb01638567ca1e2de86f12c2f359bcba7319
SnehalChalawadi <snehalchalawadi2005@gmail.com>
2026-09-16 18:41:23 +0530

    LC-jewels-and-stones [Runtime: N/A; Memory: N/A]

--- hash-table/jewels-and-stones.java
+++ hash-table/jewels-and-stones.java
@@ -2,11 +2,9 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         HashSet<Character> set=new HashSet<>();
         for(int i=0;i<jewels.length();i++){
-            if(!set.contains(jewels.charAt(i)))
-            {
-                set.add(jewels.charAt(i));
-            }
+            set.add(jewels.charAt(i));
         }
+        
         int count=0;
         for(int i=0;i<stones.length();i++)
         {
