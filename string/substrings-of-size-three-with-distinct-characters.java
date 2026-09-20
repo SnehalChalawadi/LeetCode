50f543adec7e530818dfb757d7a1bec2c5e6e437
SnehalChalawadi <snehalchalawadi2005@gmail.com>
2026-09-20 15:20:00 +0530

    LC-substrings-of-size-three-with-distinct-characters [Runtime: N/A; Memory: N/A]

--- string/substrings-of-size-three-with-distinct-characters.java
+++ string/substrings-of-size-three-with-distinct-characters.java
@@ -1,22 +1,8 @@
 class Solution {
     public int countGoodSubstrings(String s) {
         int count=0;
-        int left=0;
-        HashSet<Character> set=new HashSet<>();
-        for(int right=0;right<s.length();right++){
-            if((right-left+1)>3)
-            {
-                char ch=s.charAt(left);
-                set.remove(ch);
-                left++;
-            }
-            char c=s.charAt(right);
-            if(set.contains(c)){
-                left++;
-                continue;
-            }
-            set.add(c);
-            if((right-left+1)==3)
+        for(int i=0;i<=(s.length()-3);i++){
+            if(s.charAt(i)!=s.charAt(i+1) && s.charAt(i)!=s.charAt(i+2) && s.charAt(i+1)!=s.charAt(i+2))
             {
                 count++;
             }
